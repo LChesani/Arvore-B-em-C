4f0ae5a811d8306dafe60c492f8cec0eac82d64e
LChesani <92533013+LChesani@users.noreply.github.com>
2022-08-01 18:15:39 -0300

    Create Menu.h

---
+++ Menu.h
@@ -0,0 +1,8 @@
+#include "BTree.h"
+
+typedef struct cachorro Cachorro;
+
+int le_op();
+void inclui(FILE *arq, ArvB *arv);
+void mostra_todos(FILE *arq);
+void selecionar(int op, FILE *arq, ArvB *arv);
