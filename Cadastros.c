2d51f00e2d9635ae415192d7f9296235302570c4
LChesani <92533013+LChesani@users.noreply.github.com>
2022-08-01 00:44:15 -0300

    Update Cadastros.c

--- Cadastros.c
+++ Cadastros.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "BTree.h"
-
 
 int main(void){
 
@@ -12,21 +12,15 @@ int main(void){
 
 
 
-    Cachorro *c = (Cachorro*)malloc(sizeof(Cachorro));
-    c->id = 1;
-    strcpy(c->nome, "Bobby");
-    strcpy(c->raca, "Labrador");
-    c->idade = 2;
-    ArvB_insere(arv, c);
+    Indice *idx;
+    
+
     ArvB_printa(arv);
 
 
 
 
 
-
-
-
-    ArvB_limpa(arv);
+    //ArvB_limpa(arv);
     return 0;
 }
