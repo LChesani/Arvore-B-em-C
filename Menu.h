ad48d3cfb301e9fbda8fdad3d7ff6db172d03506
LChesani <92533013+LChesani@users.noreply.github.com>
2022-08-01 19:19:04 -0300

    Update Menu.h

--- Menu.h
+++ Menu.h
@@ -7,3 +7,4 @@ void inclui(FILE *arq, ArvB *arv);
 void mostra_todos(FILE *arq);
 void selecionar(int op, FILE *arq, ArvB *arv);
 ArvB *importa_arvore(FILE *arq);
+void pesquisa_arq(FILE *arq, Indice *idx);
