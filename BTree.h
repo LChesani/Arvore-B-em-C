dfc35143a634d31f4e61e7945c4ab677cb3d4cfe
LChesani <92533013+LChesani@users.noreply.github.com>
2022-08-01 19:18:11 -0300

    Update BTree.h

--- BTree.h
+++ BTree.h
@@ -3,7 +3,6 @@ typedef struct indice Indice;
 ArvB *ArvB_cria(void);
 void ArvB_limpa(ArvB *arv);
 void ArvB_insere(ArvB *arv, Indice *idx);
-void ArvB_printa(ArvB *arv);
 Indice *Arv_busca(ArvB *arv, Indice *idx);
 Indice *indice_cria(int id, int pos_seek);
 int getPos(Indice *idx);
