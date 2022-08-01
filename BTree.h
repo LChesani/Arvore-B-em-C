4e1a6a1ecf3a947f8a5db84d480ac61972550c7e
LChesani <92533013+LChesani@users.noreply.github.com>
2022-07-31 22:41:40 -0300

    Delete BTree.h

--- BTree.h
+++
@@ -1,6 +0,0 @@
-typedef struct btree BTree;
-BTree *BTreeCreate(void);
-BTree *BTreeFree(BTree *arv);
-BTree *BTreeInsert(BTree *arv, int ch);
-void BTreePrint(BTree *arv);
-Btree *BTreeSearch(BTree *arv, int ch);
