2c108942ec27dd2a708ac4c6a8ff1dc77b7bc8e3
LChesani <92533013+LChesani@users.noreply.github.com>
2022-08-01 19:18:28 -0300

    Update Main.c

--- Main.c
+++ Main.c
@@ -4,22 +4,21 @@
 #include "BTree.h"
 #include "Menu.h"
 #include <unistd.h>
+
+//Trabalho por Joao Davi Mazzarolo e Luis Henrique Chesani
+
 int main(void){
-
-
     FILE *arq;
     int op;
     if (access("canil.dat", F_OK) == 0)
-        arq = fopen("canil.dat", "r+"); // arquivo existe
+        arq = fopen("canil.dat", "r+");
     else
-        arq = fopen("canil.dat", "w+"); // arquivo nao existia
+        arq = fopen("canil.dat", "w+");
     ArvB *arv = importa_arvore(arq);
-    
+
     do{
         op = le_op();
         selecionar(op, arq, arv);
-
-
     }while(op != 4);
 
     fclose(arq);
